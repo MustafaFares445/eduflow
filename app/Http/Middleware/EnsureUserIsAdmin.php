dfbcf47c25fc73a4dbc8b6ca59f3650d6dc25762
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-24 04:58:47 +0300

    Simplify admin middleware response type

--- app/Http/Middleware/EnsureUserIsAdmin.php
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class EnsureUserIsAdmin
 {
-    public function handle(Request $request, Closure $next): Response|JsonResponse
+    public function handle(Request $request, Closure $next): Response
     {
         if ((bool) $request->user()?->is_admin) {
             return $next($request);
