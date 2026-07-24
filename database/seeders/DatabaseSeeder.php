b595a7ecefad240871567719901e65cbd3574980
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-24 04:10:45 +0300

    Run mobile demo seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,12 +10,12 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        $this->call(EduFlowDemoSeeder::class);
+        $this->call([
+            EduFlowDemoSeeder::class,
+            MobileDemoSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
